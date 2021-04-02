471acc863a3c09e4aea27018c9565e7e0a5635ca
WalukaUI <cwaluka@yahoo.com>
2021-04-01 22:45:08 -0700

    countdown

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(array,messsage){
+    let all =[]
+  for(let i=0;i<array.length;i++){
+    
+    all.push(`Thank you, ${array[i]}, for the wonderful ${messsage} gift!`);
+  }
+  return all
+  }
+  function countDown(a){
+      console.log(a);
+    for(let i=a;i>0;i--){
+        console.log(i-1);
+    }
+}
